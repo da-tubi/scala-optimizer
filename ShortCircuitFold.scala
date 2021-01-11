9ca04019b1835248d65bc1fcf2d9a9106a754e1b
Da <da@tubi.tv>
2021-01-11 12:02:08 +0800

    Update

--- ShortCircuitFold.scala
+++ ShortCircuitFold.scala
@@ -1,12 +1,18 @@
 object ShortCircuitFold {
   def main(args: Array[String]): Unit = {
-    val x = 1000
-    val y = 1024
-    val x1 = if (true) x else (1 to 100000).sum
-    val y1 = if (false) (1 to 100000).sum else y
-    println(x)
-    println(y)
-    println(x1)
-    println(y1)
+    val x = ((1 to 100).sum == 5050)
+    val y = ((1 to 100).sum == 5051)
+    val c1 = true || x == true
+    val c2 = false && y == false
+    val c3 = true && x == x
+    val c4 = false || y == y
+    val c5 = x && true == x
+    val c6 = y || false == y
+    println(c1)
+    println(c2)
+    println(c3)
+    println(c4)
+    println(c5)
+    println(c6)
   }
 }
