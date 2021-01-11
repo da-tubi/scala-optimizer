1656094800c0f9faa6069c6d5cba1cd7ae4c2e83
Da <da@tubi.tv>
2021-01-11 11:15:01 +0800

    wip

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 ## Basic
 All Scala compiler phases:
 ```
-sbt 'set scalaVersion := "2.13.4"' 'set scalacOptions in ThisBuild ++= Seq("-Ydebug")' compile
+sbt 'set scalaVersion := "2.13.4"' 'set scalacOptions in ThisBuild ++= Seq("-Ydebug")' clean compile
 ```
-Enable logging for the specific phase:
+Enable logging for the specific phase(eg. the jvm phase):
 ```
-sbt 'set scalaVersion := "2.13.4"' 'set scalacOptions in ThisBuild ++= Seq("-Ydebug", "-Ylog:jvm")' compile
+sbt 'set scalaVersion := "2.13.4"' 'set scalacOptions in ThisBuild ++= Seq("-Ydebug", "-Ylog:jvm")' clean compile
+```
+Enable logging for the specific phase(eg. the typer phase):
+```
+sbt 'set scalaVersion := "2.13.4"' 'set scalacOptions in ThisBuild ++= Seq("-Ydebug", "-Ylog:typer")' clean compile
+```
+javap
+```
+javap -constants -c -cp target/scala-2.13/classes "Test$"
+```
+
+## Customized Scala Compiler
+```
+ sbt 'set scalaVersion := "2.13.5-bin-SNAPSHOT"' clean compile
 ```
--- constant_opt.scala
+++ constant_opt.scala
@@ -1,62 +1,8 @@
 object Test extends App {
-  def testBothReachable(): Unit = {
-    val i = util.Random.nextInt()
-    val x = if (i % 2 == 0) null else "good"
-    val y = if (x == null) "good" else x + ""
-    println(s"testBothReachable: $y")
+  val a = 1 + 1
+  if (1 + 1 == 2) {
+    println("Yes")
+  } else {
+    println((1 to 100000).sum)
   }
-
-  def testOneReachable(): Unit = {
-    val i = 1
-    val x = if (i != 1) null else "good"
-    val y = if (x == null) "good" else x + ""
-    println(s"testOneReachable: $y")
-  }
-
-  def testAllReachable(): Unit = {
-    val i = util.Random.nextInt()
-    val y = (i % 2) match {
-      case 0 => "good"
-      case 1 => "good"
-      case _ => "good"
-    }
-    println(s"testAllReachable: $y")
-  }
-
-  def testOneUnreachable(): Unit = {
-    val i = util.Random.nextInt()
-    val x = if (i % 2 == 0) {
-      1
-    } else {
-      2
-    }
-    val y = x match {
-      case 0 => "good"
-      case 1 => "good"
-      case _ => "good"
-    }
-    println(s"testOneUnreachable: $y")
-  }
-
-  def testDefaultUnreachable(): Unit = {
-    val i = util.Random.nextInt()
-    val x = if (i % 2 == 0) {
-      1
-    } else {
-      2
-    }
-    val y = x match {
-      case 1 => "good"
-      case 2 => "good"
-      case _ => "good"
-    }
-    println(s"testDefaultUnreachable: $y")
-  }
-
-  testBothReachable()
-  testOneReachable()
-  testAllReachable()
-  testOneUnreachable()
-  testDefaultUnreachable()
 }
-
